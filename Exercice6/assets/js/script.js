f2d3604c868a96d70f1bee3ad8f540bcc8085ac6
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-06 10:02:16 +0200

    exercice 6 fini

---
+++ Exercice6/assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#link').mouseover(function() {
+        $('#link').css({ width: '800px'});
+    });
+    $('#link').mouseout(function() {
+        $('#link').css({ width: '676px'});
+    });
+});
