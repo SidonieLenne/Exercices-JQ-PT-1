db406ce2bb7aca81c9a9eb80ee5cdd8e1e1656cf
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-07 12:07:23 +0200

    exercice 9 corrigé

--- Exercice9/assets/js/script.js
+++ Exercice9/assets/js/script.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    $('#green').click(function(){
-        $('#text').css('color', 'green');
-      });
-      $('#red').click(function(){
-        $('#text').css('color', 'red');
-      });
-      $('#blue').click(function(){
-        $('#text').css('color', 'blue');
-      });
+  /*$('#green').click(function(){
+    $('#text').css('color', 'green');
+  });
+  $('#red').click(function(){
+    $('#text').css('color', 'red');
+  });
+  $('#blue').click(function(){
+    $('#text').css('color', 'blue');
+  });*/
+
+  /*$('.color').click(function(){
+    var color = $(this).attr('id');
+    $('#text').css('color',color);
+  });*/
+  
+/*On cible tous les éléments avec la classe color. Au clic, la fonction se déclenche:
+Cette fonction cible tous les éléments avec l'ID text, et elle change leur css.
+Ce css change la couleur par l'id de l'élément ciblé (par le clic).  */
+  $('.color').click(function(){
+    $('#text').css('color',this.id);
+  });
 });
